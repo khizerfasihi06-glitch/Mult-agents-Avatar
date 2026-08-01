8d82314908ab6505bd51241a48d42dc9f1837cab
Khizar Fasihi <khizerfasihi06@gmail.com>
2026-08-02 02:06:13 +0500

    Refactor UI2.py to improve chatbot functionality

--- UI2.py
+++ UI2.py
@@ -2,6 +2,8 @@ import os
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from langchain_mistralai import ChatMistralAI
+import io
+from pypdf import PdfReader
 
 # --- Page Configuration ---
 st.set_page_config(page_title="Multi-persona AI Chatbot", layout="centered")
